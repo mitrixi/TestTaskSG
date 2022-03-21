b0814c808551baf672bab3005661a35bbe706058
mitrixi <balalindmitry@gmail.com>
2022-03-21 20:46:11 +0300

    init commit

--- src/main/java/com/autotests/TestData.java
+++ src/main/java/com/autotests/TestData.java
@@ -1,8 +1,5 @@
 package com.autotests;
 
-/**
- *
- */
 public class TestData {
     public static final String BASIC_AUTHENTICATION_USERNAME = "front_2d6b0a8391742f5d789d7d915755e09e";
     public static final String BASIC_AUTHENTICATION_PASSWORD = "";
